6e34719b16ecf018132c1d121205297cd1e593cc
bdshakhawat <mdshakwat.hossain@gmail.com>
2024-01-24 23:06:45 +0600

    modal commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,14 @@
-import { UserButton } from "@clerk/nextjs";
+'use client'
+import { Modal } from "@/components/ui/modal";
+// import { UserButton } from "@clerk/nextjs";
 
 
 const SetupPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <UserButton afterSignOutUrl="/" />
+    <div>
+      <Modal title="Setup" description="Setup your account" isOpen onClose={() => {}}>
+        Children
+      </Modal>
     </div>
   )
 }
